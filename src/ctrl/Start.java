c893dc8ac99ba728652c6ea4139d3ff30c85dbed
zinamaki <zakimina3@gmail.com>
2017-03-13 13:59:57 -0400

    Refactor

--- src/ctrl/Start.java
+++ src/ctrl/Start.java
@@ -13,47 +13,50 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Start")
 public class Start extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Start() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		boolean registerPressed = "Register".equals(request.getParameter("register"));
-		boolean loginPressed = "Login".equals(request.getParameter("login"));
-		
-		if(registerPressed){
-			System.out.println("Registration button pressed");
-			request.getRequestDispatcher("/Register.jspx").forward(request, response);
-		}else if(loginPressed){
-			System.out.println("Login button pressed");
-			request.getRequestDispatcher("/Login.jspx").forward(request, response);
-		}else{
-			request.getRequestDispatcher("/Login.jspx").forward(request, response);
-		}
-		
-		System.out.println("Username is: " + username + " Password is: " + password);
-		
+	public Start() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		boolean registerPressed = "Register".equals(request.getParameter("register"));
+		boolean loginPressed = "Login".equals(request.getParameter("login"));
+
+		if (registerPressed) {
+			System.out.println("Registration button pressed");
+			request.getRequestDispatcher("/Register.jspx").forward(request, response);
+		} else if (loginPressed) {
+			System.out.println("Login button pressed");
+			request.getRequestDispatcher("/Login.jspx").forward(request, response);
+		} else {
+			request.getRequestDispatcher("/Login.jspx").forward(request, response);
+		}
+
+		System.out.println("Username is: " + username + " Password is: " + password);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
