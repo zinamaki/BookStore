620e5fa02e01e31c0952151c025ab5fdc0865c4e
papaiza <papaiza_12@hotmail.com>
2017-03-13 13:19:30 -0400

    not hi

--- src/ctrl/Start.java
+++ src/ctrl/Start.java
@@ -28,7 +28,6 @@ public class Start extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("HI");
 	}
 
 	/**
