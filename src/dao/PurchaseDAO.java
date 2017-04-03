b2e50b6464263040419cb51180b958793387eab9
zinamaki <zakimina3@gmail.com>
2017-04-03 06:11:34 -0400

    Updated purchase DAO to insert into PO table

--- src/dao/PurchaseDAO.java
+++ src/dao/PurchaseDAO.java
@@ -1,30 +1,47 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import bean.CartBean;
+import bean.BookBean;
 
 public class PurchaseDAO {
+
 	private DataSource ds;
-	
-	public PurchaseDAO() throws ClassNotFoundException{
-		try{
+
+	public PurchaseDAO() throws ClassNotFoundException {
+		try {
 			ds = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
-		}catch(NamingException e){
+		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public String subitOrder(int pid, String status, ArrayList<CartBean> list) throws SQLException {
-		String result = "";
-//		try{
-//			
-//		}
-		return result;
+
+	public boolean addOrder(String email, String status, String address) {
+
+		try {
+			Connection con = this.ds.getConnection();
+			Statement st = con.createStatement();
+
+			// Insert a new user into the Users table
+			String updateUser = "INSERT INTO PO (email, status, address) VALUES ('" + email + "', '" + status + "', '"
+					+ address + "')";
+			st.executeUpdate(updateUser);
+			st.close();
+
+			con.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
 	}
 }
