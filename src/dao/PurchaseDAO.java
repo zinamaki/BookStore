c60b4c85217a7a9d17663ccdd0bda3caf9c4f346
zinamaki <zakimina3@gmail.com>
2017-04-03 11:37:58 -0400

    Editted purchase DAO

--- src/dao/PurchaseDAO.java
+++ src/dao/PurchaseDAO.java
@@ -25,23 +25,81 @@ public class PurchaseDAO {
 		}
 	}
 
-	public boolean addOrder(String email, String status, String address) {
-
-		try {
+	public boolean submitOrder(String email, Map<BookBean, Integer> cart) throws SQLException {
+		boolean result = false;
+		try{
 			Connection con = this.ds.getConnection();
-			Statement st = con.createStatement();
-
-			// Insert a new user into the Users table
-			String updateUser = "INSERT INTO PO (email, status, address) VALUES ('" + email + "', '" + status + "', '"
-					+ address + "')";
-			st.executeUpdate(updateUser);
-			st.close();
-
+			Statement sta = con.createStatement();
+			
+			// get the number of rows from the result set
+			ResultSet r = sta.executeQuery("SELECT count(*) FROM po");
+			r.next();
+			int rowCount = r.getInt(1);
+			String status;
+			if(rowCount % 3 == 0){
+				
+				status = "DENIED"; 
+			}else{
+				result = true;
+				status = "ORDERED";
+			}
+			r.close();
+			
+			int addressId = retrieveAddressId(sta, email);
+			
+			// Update the PO table
+			int poId = submitPO(sta, email, status, addressId);
+			
+			submitPOItems(sta, cart, poId);
+			
+			sta.close();
 			con.close();
-			return true;
-		} catch (Exception e) {
-			return false;
+			
+		}catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			e.printStackTrace();
 		}
+		return result;
+	}
 
+	public int retrieveAddressId(Statement sta, String email) throws SQLException {
+		ResultSet r2 = sta.executeQuery("select id from address where email='" + email + "'");
+		r2.next();
+		int addressId = r2.getInt(1);
+		r2.close();
+		return addressId;
+	}
+	
+	public int submitPO(Statement sta, String email, String status, int addressId) throws SQLException{
+		String addPO = "INSERT INTO PO (email, status, address) VALUES ('" + email + "', '" + status + "', " + addressId + ")";  
+		sta.executeUpdate(addPO);
+		String query = "select id from PO where email = '" + email + "'and status = '" + status + "' and address = " + addressId;
+		ResultSet r = sta.executeQuery(query);
+		r.next();
+		int id = r.getInt(1);
+		r.close();
+		return id;
+	}
+	
+	public void submitPOItems(Statement sta, Map<BookBean, Integer> cart, int poId) throws SQLException {
+		for (Map.Entry<BookBean, Integer> item : cart.entrySet()){
+			String update = "INSERT INTO POItem (id, title, author, price, quantity) VALUES ("
+						+ poId + ", '" + item.getKey().getTitle() + "', '" + item.getKey().getAuthor()
+						+ "', " + item.getKey().getPrice() + ", " + item.getValue() + ")";
+			sta.executeUpdate(update);
+		}
+	}
+	
+	public void updateStatus() throws SQLException {
+		Connection con = this.ds.getConnection();
+
+		Statement stmt = con.createStatement();
+
+		String sql = "UPDATE PO " + "SET status='denied' WHERE MOD(ind, 3) = 0";
+		stmt.executeUpdate(sql);
+		sql = "UPDATE PO " + "SET status='processed' WHERE status='ordered'";
+		stmt.executeUpdate(sql);
+		stmt.close();
+		con.close();
 	}
 }
